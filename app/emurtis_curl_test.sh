46cff4e8fe775d6fa1cd27e96002124331181ac9
Konner Curtis <konner.curtis@populusgs.com>
2021-03-28 20:22:27 -0300

    Updated curl test script

--- app/emurtis_curl_test.sh
+++ app/emurtis_curl_test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 curl -i -H "Content-Type: application/json" -X POST -d '{"username": "<unb_id>", "password": "<unb_password>"}' -c cookie-jar -k https://cs3103.cs.unb.ca:50035/users/login
 
+curl -i -H "Content-Type: application/json" -X POST -d '{"username": "<unb_id>"}' -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users
+
 curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users
 
 curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users?username=test
 
-curl -i -H "Content-Type: application/json" -X POST -d '{"username": "testuser"}' -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users
+curl -i -X POST -H "vidName: bigName" -H  "vidDesc: desc" -F "video=@<full_path_to_file>" -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/1/videos
 
 curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/1/videos/1
 
 curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/1/videos
 
 curl -i -H "Content-Type: application/json" -X DELETE -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/1/videos/1
-
-curl -i -H "vidName: bigName" -H  "vidDesc: desc" -F "video=@<full_path_to_file>" -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/1/videos
 
 curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/logout
 
