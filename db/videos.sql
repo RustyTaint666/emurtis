71c90735f4da7dd10182521c912c32f4e1becb0c
Konner Curtis <konner.curtis@populusgs.com>
2021-03-28 14:55:29 -0300

    Updated videos table to store file paths.

--- db/videos.sql
+++ db/videos.sql
@@ -3,7 +3,7 @@ CREATE TABLE videos (
     video_id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     name varchar(250) NOT NULL,
-    video LONGBLOB NOT NULL,
+    video TEXT NOT NULL,
     description TEXT,
     PRIMARY KEY (video_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
