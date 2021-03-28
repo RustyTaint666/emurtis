b617e44f8935de6c86e04cc3fea4aa65ca6bc548
Konner Curtis <konner.curtis@populusgs.com>
2021-03-28 20:02:47 -0300

    Added valid video post to curl script.

--- app/emurtis_curl_test.sh
+++ app/emurtis_curl_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -i -H "Content-Type: application/json" -X POST -d '{"username": "kcurtis2", "password": "Ronniejamesdio1!"}' -c cookie-jar -k https://cs3103.cs.unb.ca:50035/users/login
+curl -i -H "Content-Type: application/json" -X POST -d '{"username": "<unb_id>", "password": "<unb_password>"}' -c cookie-jar -k https://cs3103.cs.unb.ca:50035/users/login
 
 curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users
 
@@ -13,4 +13,7 @@ curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs31
 
 curl -i -H "Content-Type: application/json" -X DELETE -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/1/videos/1
 
+curl -i -H "vidName: bigName" -H  "vidDesc: desc" -F "video=@<full_path_to_file>" -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/1/videos
+
 curl -i -H "Content-Type: application/json" -X GET -b cookie-jar -k https://cs3103.cs.unb.ca:50035/users/logout
+
